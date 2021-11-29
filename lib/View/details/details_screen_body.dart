dce9f13b03e915d09a74d4f4e0a643f685dd5a82
alllowd <philip.a.bramwell@student.fh-kiel.de>
2021-11-29 14:37:02 +0100

    Api_details - high ohlc value added to Ohlc Class so it can be used in details_screen_body.dart

--- lib/View/details/details_screen_body.dart
+++ lib/View/details/details_screen_body.dart
@@ -53,6 +53,8 @@ class _DetailsScreenBody extends State<DetailsScreenBody> {
           " " +
           ohlc.open.toString() +
           " " +
+          ohlc.high.toString() +
+          " " +
           ohlc.low.toString() +
           " " +
           ohlc.close.toString()),
