7c1f379fb96d5c95c86c34cd6d393ece4b2f8354
Dominik Grohs <dominik.grohs@student.fh-kiel.de>
2021-12-15 09:36:13 +0100

    single-item-diagrams_tried to use the ohlc-data to display a chart

--- lib/View/details/details_screen_body.dart
+++ lib/View/details/details_screen_body.dart
@@ -2,6 +2,7 @@ import 'package:crypto_lab/Model/crypto.dart';
 import 'package:crypto_lab/Model/ohlc.dart';
 import 'package:crypto_lab/Controller/ohlc_history_api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class DetailsScreenBody extends StatefulWidget {
   final Crypto _crypto;
@@ -14,21 +15,40 @@ class DetailsScreenBody extends StatefulWidget {
 
 class _DetailsScreenBody extends State<DetailsScreenBody> {
   final OhlcHistoryApiService _ohlcHistoryApiService = OhlcHistoryApiService();
-  late Future<List<Ohlc>> _ohlcList;
+  static List<Ohlc>? _ohlcList;
 
-  @override
-  void initState() {
-    super.initState();
+  Future<void> setOhlcList() async {
     // Get Crypto Instance which got tapped and got passed as argument
     // check _createListViewItems in overview_screen_body.dart
     // Make Api call to get crypto coins and store Future in _cryptoList for later use
     // pass "no valid id to the ohlc API call if crypto id is null => Api call will not return a result
-    _ohlcList = _ohlcHistoryApiService.getOhcl(widget._crypto.id ?? "no valid id");
+    _ohlcList = await _ohlcHistoryApiService.getOhcl(widget._crypto.id ?? "no valid id");
+  }
+
+  Future<List<LineSeries<Ohlc, num>>> getLineSeries() async {
+    await setOhlcList();
+    return <LineSeries<Ohlc, num>>[
+      LineSeries<Ohlc, num>(
+        dataSource: _ohlcList!,
+        xValueMapper: (_ohlcList, _) => _ohlcList.time,
+        yValueMapper: (_ohlcList, _) => _ohlcList.high,
+        width: 2,
+      ),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
+    String crypto = widget._crypto.name ?? "Example";
     // TODO: implement Details Statistics
+    return Center(
+        child: SfCartesianChart(
+          title: ChartTitle(text: crypto + "-Chart"),
+          legend: Legend(isVisible: true),
+          series: _ohlcList,
+        )
+    );
+    /*
     return FutureBuilder(
       future: _ohlcList,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -45,9 +65,12 @@ class _DetailsScreenBody extends State<DetailsScreenBody> {
         }
       },
     );
+
+     */
   }
 
-  Widget _showDemoOhcl(Ohlc ohlc) {
+  /*Widget _showDemoOhcl(Ohlc ohlc) {
+
     return Card(
         child: ListTile(
       title: Text(ohlc.time.toString() +
@@ -61,4 +84,6 @@ class _DetailsScreenBody extends State<DetailsScreenBody> {
           ohlc.close.toString()),
     ));
   }
+
+     */
 }
