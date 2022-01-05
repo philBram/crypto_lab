8f6be4d85363fd495e4648f78962e488a325a378
Philip Alan Bramwell <philip.a.bramwell@student.fh-kiel.de>
2022-01-05 19:56:05 +0100

    news_list - added Magnetometer rotation sensor (sensor_plus and scroll_to_index dependency) to new_list.dart so it is possible to scroll throw the news list by tilting the phone in y-axis direction.

--- lib/View/home/news_list.dart
+++ lib/View/home/news_list.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:crypto_lab/Model/articles.dart';
 import 'package:crypto_lab/View/adaptive_text_size.dart';
 import 'package:crypto_lab/Controller/crypto_news_api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:crypto_lab/View/crypto_lab_colors.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class NewsList extends StatefulWidget {
@@ -23,11 +27,50 @@ class _NewsList extends State<NewsList> {
   final CryptoNewsApiService _cryptoNewsApiService = CryptoNewsApiService();
   late Future<List<Article>> _articleList;
 
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+  final _scrollDirection = Axis.horizontal;
+  late int _listLength;
+  final _yRotationThreshold = 15;
+  int _currentListPosition = 0;
+  late AutoScrollController _autoScrollController;
+
   @override
   void initState() {
     super.initState();
     // Make Api call to get Articles and store Future in _articleList for later use
     _articleList = _cryptoNewsApiService.getArticle();
+
+    _listLength = int.parse(_cryptoNewsApiService.newsArticleSize);
+
+    _autoScrollController = AutoScrollController(
+      viewportBoundaryGetter: () =>
+          Rect.fromLTRB(0, 0, 0, MediaQuery.of(context).padding.bottom),
+      axis: _scrollDirection,
+    );
+
+    // Listens to Rotation changes
+    // https://pub.dev/packages/sensors_plus
+    _streamSubscriptions.add(
+      magnetometerEvents.listen(
+        (MagnetometerEvent event) {
+          setState(() {
+            print(event.x);
+            print(_currentListPosition);
+            if (event.x > _yRotationThreshold) {
+              _currentListPosition = _currentListPosition <= 0
+                  ? _currentListPosition
+                  : _currentListPosition - 1;
+              _scrollToIndex(_currentListPosition);
+            } else if (event.x < -_yRotationThreshold) {
+              _currentListPosition = _currentListPosition > _listLength
+                  ? _currentListPosition
+                  : _currentListPosition + 1;
+              _scrollToIndex(_currentListPosition);
+            }
+          });
+        },
+      ),
+    );
   }
 
   @override
@@ -41,10 +84,11 @@ class _NewsList extends State<NewsList> {
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData) {
             return ListView.builder(
+              controller: _autoScrollController,
+              scrollDirection: _scrollDirection,
               itemCount: snapshot.data.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (_, index) =>
-                  _createListViewItem(context, snapshot.data[index]),
+              itemBuilder: (_, index) => _createListViewItem(
+                  context, snapshot.data[index], index, _autoScrollController),
             );
           } else {
             return const Center(
@@ -56,21 +100,27 @@ class _NewsList extends State<NewsList> {
     );
   }
 
-  Widget _createListViewItem(BuildContext context, Article article) {
-    return Container(
-      margin: _listItemMargin,
-      // width of listview item is % value of _listItemWidthFactor
-      width: MediaQuery.of(context).size.width * _listItemWidthFactor,
-      decoration: BoxDecoration(
-        color: CryptoLabColors.cryptoLabBackground.withOpacity(0.8),
-        border: Border.all(color: CryptoLabColors.cryptoLabFont),
-        borderRadius: _listItemBorderRadius,
-      ),
-      padding: _listItemPadding,
-      child: ListTile(
-        // article url is null => pass empty String so no website will be opened
-        onTap: () => _launchURL(article.url ?? ""),
-        title: _createListViewItemContent(article),
+  Widget _createListViewItem(BuildContext context, Article article, int index,
+      AutoScrollController autoScrollController) {
+    return AutoScrollTag(
+      index: index,
+      key: ValueKey(index),
+      controller: _autoScrollController,
+      child: Container(
+        margin: _listItemMargin,
+        // width of listview item is % value of _listItemWidthFactor
+        width: MediaQuery.of(context).size.width * _listItemWidthFactor,
+        decoration: BoxDecoration(
+          color: CryptoLabColors.cryptoLabBackground.withOpacity(0.8),
+          border: Border.all(color: CryptoLabColors.cryptoLabFont),
+          borderRadius: _listItemBorderRadius,
+        ),
+        padding: _listItemPadding,
+        child: ListTile(
+          // article url is null => pass empty String so no website will be opened
+          onTap: () => _launchURL(article.url ?? ""),
+          title: _createListViewItemContent(article),
+        ),
       ),
     );
   }
@@ -131,5 +181,10 @@ class _NewsList extends State<NewsList> {
     } else {
       throw 'Could not launch $url';
     }
+  }
+
+  Future _scrollToIndex(index) async {
+    await _autoScrollController.scrollToIndex(index,
+        preferPosition: AutoScrollPosition.begin);
   }
 }
