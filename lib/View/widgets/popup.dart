6b6212a4d17b8ca3df403e2155297b548840e438
Dominik Grohs <dominik.grohs@student.fh-kiel.de>
2022-01-06 13:59:46 +0100

    user-settings: - improved Popup

--- lib/View/widgets/popup.dart
+++ lib/View/widgets/popup.dart
@@ -7,6 +7,9 @@ enum PopupType {
   confirmation,
   success,
   failed,
+  changePassword,
+  oneStandardInputField,
+  twoStandardInputFields,
 }
 
 class Popup extends StatelessWidget {
@@ -14,8 +17,11 @@ class Popup extends StatelessWidget {
   final PopupType popupType;
   final String title;
   final String content;
+
   /// Determines if the user is confirming the popup, only then sets this to true, otherwise false.
   final Function(bool) onConfirmationCallback;
+
+  final Function(List<String>)? onConfirmationTextFieldValues;
 
   const Popup({
     required this.buildContext,
@@ -23,7 +29,9 @@ class Popup extends StatelessWidget {
     required this.title,
     required this.content,
     required this.onConfirmationCallback,
-    Key? key}) : super(key: key);
+    this.onConfirmationTextFieldValues,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) => _buildAlertDialog();
@@ -31,7 +39,13 @@ class Popup extends StatelessWidget {
   Widget _buildAlertDialog() {
     return AlertDialog(
       title: Text(title, style: const TextStyle(color: CryptoLabColors.cryptoLabFont)),
-      content: Text(content, style: const TextStyle(color: CryptoLabColors.cryptoLabFont)),
+      content: Column(
+        children: [
+          Text(content, style: const TextStyle(color: CryptoLabColors.cryptoLabFont)),
+          // _isInputFieldPopup() ? _buildInputFields() : const SizedBox.shrink(),
+          for (Widget widget in _buildInputFields()) widget,
+        ],
+      ),
       actions: [
         _isConfirmationPopup() ? _buildCancelButton() : const SizedBox.shrink(),
         _isConfirmationPopup() ? _buildConfirmationButton() : _buildRogerButton(),
@@ -40,10 +54,37 @@ class Popup extends StatelessWidget {
   }
 
   bool _isConfirmationPopup() {
-    if (popupType == PopupType.confirmation) {
+    if (popupType == PopupType.confirmation || _isInputFieldPopup()) {
       return true;
     }
     return false;
+  }
+
+  bool _isInputFieldPopup() {
+    switch (popupType) {
+      case PopupType.oneStandardInputField:
+        return true;
+      case PopupType.twoStandardInputFields:
+        return true;
+      case PopupType.passwordInputField:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  /// Builds input-field(s) if [PopupType] is [passwordInputField], [oneStandardInputField] or [twoStandardInputFields].
+  List<Widget> _buildInputFields() {
+    if (_isInputFieldPopup()) {
+      List<Widget> inputFieldWidgets = [];
+
+      if (popupType == PopupType.changePassword) {
+      } else if (popupType == PopupType.oneStandardInputField || popupType == PopupType.twoStandardInputFields) {
+        if (popupType == PopupType.twoStandardInputFields) {}
+      }
+    } else {
+      return const <Widget>[];
+    }
   }
 
   /// Builds a "Verstanden"-Button.
